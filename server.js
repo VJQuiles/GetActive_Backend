5f426b84f778394b3dd66c0886e140e9031d57a0
Vince Quiles <vjquiles@msn.com>
2025-09-15 15:03:25 -0400

    I don't know why but cors randomly popped in my head and I'm happy it did rather than running into that headache later on

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ require('dotenv').config()
 const express = require('express')
 require('./config/db-connection')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 
 app.use(express.json())
-
+app.use(cors())
 app.use(routes)
 
 app.listen(PORT, () => {
